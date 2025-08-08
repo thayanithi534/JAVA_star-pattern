a86492fc01ce7effc3c7534fd9792713aedd3c55
thayanithi534 <jthaya6@gmail.com>
2025-08-08 23:50:32 +0530

    Create star_pattern.java

---
+++ star_pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class star_pattern
+  {
+    public static void main(String args[])
+    {
+      Scanner obj=new Scanner(System.in);
+      int n=obj.nextInt();
+      System.out.print("enter the value of n:"+n);
+      for(i=1;i<=n;i++)
+        {
+          for(j=1;j<=i;j++)
+            {
+              System.out.print("*");
+            }
+          System.out,println();
+        }  
+    }
+  }  
